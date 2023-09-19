a9ab81a1e87c4f6989c5f83634410d52de30c126
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2023-09-18 21:19:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/tip_calculator_android/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.tip_calculator_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tip_calculator_android.ui.theme.Tip_CalculatorAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Tip_CalculatorAndroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Tip_CalculatorAndroidTheme {
+        Greeting("Android")
+    }
+}
