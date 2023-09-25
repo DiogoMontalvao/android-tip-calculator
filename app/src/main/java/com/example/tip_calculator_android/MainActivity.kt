6309c199c3c8910a2aeabb652b892f0a83c8c1e5
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2023-09-25 00:07:30 -0300

    Calculate the tip - 4. Calculate the tip

--- app/src/main/java/com/example/tip_calculator_android/MainActivity.kt
+++ app/src/main/java/com/example/tip_calculator_android/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.tip_calculator_android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import kotlin.math.*
+import com.example.tip_calculator_android.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
+        binding.buttonCalcular.setOnClickListener { calcularGorjeta() }
+    }
+
+    fun calcularGorjeta() {
+        val stringCampoDeTexto = binding.inputValorDaConta.text.toString()
+        val valor = stringCampoDeTexto.toDouble()
+
+        val opcaoSelecionada = binding.groupOpcoesGorjeta.checkedRadioButtonId
+        val porcentagemGorjeta = when (opcaoSelecionada){
+            R.id.radio_vinte_porcento -> 0.20
+            R.id.radio_quinze_porcento -> 0.15
+            else -> 0.10
+        }
+
+        var gorjeta = valor * porcentagemGorjeta
+        val arredondarParaCima = binding.switchArredondarParaCima.isChecked
+        if (arredondarParaCima) gorjeta = ceil(gorjeta)
+
+        val gorjetaFormatada = NumberFormat.getCurrencyInstance().format(gorjeta)
+        binding.textValorGorjeta.text = getString(R.string.valor_da_gorjeta, gorjetaFormatada)
+
     }
 }
