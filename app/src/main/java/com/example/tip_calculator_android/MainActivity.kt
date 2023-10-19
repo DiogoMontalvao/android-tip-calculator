d1fc34d428aadfcf0e1d471385e2f9305c66b50d
DiogoMontalvao <109600744+DiogoMontalvao@users.noreply.github.com>
2023-10-19 19:12:05 -0300

    Calculate the tip - Final

--- app/src/main/java/com/example/tip_calculator_android/MainActivity.kt
+++ app/src/main/java/com/example/tip_calculator_android/MainActivity.kt
@@ -7,7 +7,7 @@ import com.example.tip_calculator_android.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,23 +17,32 @@ class MainActivity : ComponentActivity() {
         binding.buttonCalcular.setOnClickListener { calcularGorjeta() }
     }
 
-    fun calcularGorjeta() {
+    private fun calcularGorjeta() {
         val stringCampoDeTexto = binding.inputValorDaConta.text.toString()
-        val valor = stringCampoDeTexto.toDouble()
+        val valor = stringCampoDeTexto.toDoubleOrNull()
 
-        val opcaoSelecionada = binding.groupOpcoesGorjeta.checkedRadioButtonId
-        val porcentagemGorjeta = when (opcaoSelecionada){
+        if (valor == null) {
+            formatarGorjeta(0.0)
+            return
+        }
+
+        val porcentagemGorjeta = when (binding.groupOpcoesGorjeta.checkedRadioButtonId){
             R.id.radio_vinte_porcento -> 0.20
             R.id.radio_quinze_porcento -> 0.15
             else -> 0.10
         }
 
         var gorjeta = valor * porcentagemGorjeta
-        val arredondarParaCima = binding.switchArredondarParaCima.isChecked
-        if (arredondarParaCima) gorjeta = ceil(gorjeta)
 
-        val gorjetaFormatada = NumberFormat.getCurrencyInstance().format(gorjeta)
+        if (binding.switchArredondarParaCima.isChecked) {
+            gorjeta = ceil(gorjeta)
+        }
+
+        formatarGorjeta(gorjeta)
+    }
+
+    private fun formatarGorjeta(value: Double) {
+        val gorjetaFormatada = NumberFormat.getCurrencyInstance().format(value)
         binding.textValorGorjeta.text = getString(R.string.valor_da_gorjeta, gorjetaFormatada)
-
     }
 }
